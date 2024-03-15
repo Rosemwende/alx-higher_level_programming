839590e63c2a70a046fc201170675f6e74d83c84
Rosemwende <rosemuema82@gmail.com>
2024-03-15 10:46:32 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for i in matrix:
+            for a in i:
+                print("{:d}".format(a), end=' ' if a != i[-1] else '')
+                print()
