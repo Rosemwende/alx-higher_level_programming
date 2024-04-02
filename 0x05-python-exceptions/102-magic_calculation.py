2b0689e62bc9e765c1a32be50ab1932e31acb491
Rosemwende <rosemuema82@gmail.com>
2024-04-02 05:41:06 -0700

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception("Too far")
-            else
+            else:
             result += a ** b / i
         except Exception:
             result = b + a
