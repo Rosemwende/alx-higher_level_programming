f33601b19ab5ec1cbb312aff8033aa495098dfc5
Rosemwende <rosemuema82@gmail.com>
2024-04-12 13:59:25 -0700

    102-python.c

---
+++ 0x07-python-test_driven_development/tests/102-python.c
@@ -0,0 +1,28 @@
+#include <Python.h>
+
+void print_python_string(PyObject *p)
+{
+    Py_ssize_t length;
+    wchar_t *value;
+
+    printf("[.] string object info\n");
+
+    if (!PyUnicode_Check(p))
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
+
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+        printf("  type: compact ascii\n");
+    else if (PyUnicode_IS_COMPACT(p))
+        printf("  type: compact unicode object\n");
+    else
+        printf("  type: string object\n");
+
+    length = PyUnicode_GET_LENGTH(p);
+    value = PyUnicode_AsWideCharString(p, &length);
+    printf("  length: %zd\n", length);
+    printf("  value: %ls\n", value);
+    PyMem_Free(value);
+}
