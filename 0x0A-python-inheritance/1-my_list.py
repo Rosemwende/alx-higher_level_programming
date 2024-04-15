84855b2ce3f715cbde7182e3bb14f27ec3cfebdc
Rosemwende <rosemuema82@gmail.com>
2024-04-15 11:22:07 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a class MyList that inherits from list"""
+
+class MyList(list):
+    """A class that inherits from list"""
+
+    def print_sorted(self):
+        """Print the list sorted in ascending order"""
+        print(sorted(self))
