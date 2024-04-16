7a686c95aaac1de6fe36c384010b57fcb6055c99
Rosemwende <rosemuema82@gmail.com>
2024-04-16 11:22:58 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Reads a text file (UTF8) and prints it to stdout"""
+"""Reads a text file (UTF8) and prints it to stdout"""
 
 def read_file(filename=""):
     """Reads a file and prints its content"""
