0ffac50cd0f1fe551a254bccbeaf992d19d6bec3
Rosemwende <rosemuema82@gmail.com>
2024-04-23 09:56:47 -0700

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""Write the class Rectangle that inherits from Base"""
+""" Rectangle class module """
 
 from models.base import Base
 
 class Rectangle(Base):
-    """Rectangle class inheriting from Base"""
-
+    """ Rectangle class """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes the Rectangle instance"""
+        """ Initializes the instance """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-        @property
-    def width(self):
-        """Getter for width attribute"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter for width attribute"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
+    def area(self):
+        """ Returns the area of the rectangle """
+        return self.width * self.height
