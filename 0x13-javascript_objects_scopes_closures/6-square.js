e298ef922d400c1809ea8734281b5efa494583bd
Rosemwende <rosemuema82@gmail.com>
2024-06-19 14:17:19 +0300

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 /* a class Square that defines a square and inherits from Square of 5-square.js */
+
 const SquareBase = require('./5-square');
 
 class Square extends SquareBase {
   charPrint (c) {
-      if (c === undefined) {
+    if (c === undefined) {
       c = 'X';
     }
-     for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width)
-     }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
 }
 
