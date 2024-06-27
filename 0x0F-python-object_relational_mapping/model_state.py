904be1393ddd1519188b7bb41d38f39fe1c368e4
Rosemwende <rosemuema82@gmail.com>
2024-06-27 13:17:57 +0300

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,3 +15,14 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    if __name__ == "__main__":
+        import sys
+        from sqlalchemy.orm import sessionmaker
+
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+
+        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}', pool_pre_ping=True)
+        Base.metadata.create_all(engine)
